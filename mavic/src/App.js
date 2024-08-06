4937a0f3dc452ff442b13b1db59b4961772d8744
ArierEdvolikice <eobx@hotmail.com>
2024-08-06 13:43:59 -0300

    ajustes

--- mavic/src/App.js
+++ mavic/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Importando o CSS para a página
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navigation">
+        <a href="#section1">Seção 1</a>
+        <a href="#section2">Seção 2</a>
+        <a href="#section3">Seção 3</a>
+      </nav>
+      
+      <div id="section1" className="section section1">
+        <h1>Seção 1</h1>
+        <p>Conteúdo da primeira seção.</p>
+      </div>
+      
+      <div id="section2" className="section section2">
+        <h1>Seção 2</h1>
+        <p>Conteúdo da segunda seção.</p>
+      </div>
+      
+      <div id="section3" className="section section3">
+        <h1>Seção 3</h1>
+        <p>Conteúdo da terceira seção.</p>
+      </div>
     </div>
   );
 }
