a51302b71a7b4e3296844f8005908d95e3729f2b
ArierEdvolikice <eobx@hotmail.com>
2024-08-07 11:05:08 -0300

    Ajustes-Erick

--- mavic/src/App.js
+++ mavic/src/App.js
@@ -128,7 +128,7 @@ function App() {
   <div id="section4" className="section section4">
     <h1>Seção 4</h1>
     <p>Conteúdo da quarta seção.</p>
-    <p> ERICK GATÃO</p>
+    <p>ERICK GATÃO E BONITÃO</p>
   </div>
 
   <div id="section5" className="section section2">
