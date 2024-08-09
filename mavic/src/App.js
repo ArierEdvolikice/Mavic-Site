45da076285de4565d4d0a4f96b6c5dfe5f84705d
ArierEdvolikice <eobx@hotmail.com>
2024-08-08 23:39:48 -0300

    Ajustes

--- mavic/src/App.js
+++ mavic/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import imagemFundo01 from './Imagens/Imagem_Fundo_01.jpg';
 import imagemFundo02 from './Imagens/Imagem_Fundo_02.jpg';
 import imagemFundo03 from './Imagens/Imagem_Fundo_03.jpg';
+import Slider from 'react-slick';
+
+
 
 function App() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -54,132 +57,205 @@ function App() {
   useEffect(() => {
     const intervalId = setInterval(nextSlide, 10000);
 
-    return () => clearInterval(intervalId); // Limpa o intervalo ao desmontar o componente
+    return () => clearInterval(intervalId);
   }, []);
 
-  return (
-<div className="App">
-  {isMenuOpen && <div className="overlay" onClick={closeMenu}></div>}
-  <div className="section section1">
-    <div className="header">
-      <div className="menu-icon" onClick={toggleMenu}>
-        &#9776;
-      </div>
-      <div className="logo">
-        <h1>Mavic</h1>
-      </div>
-      <div className="contact-button">
-        <button onClick={() => alert('Contato')}>contate-nos</button>
-      </div>
-    </div>
-
-    <div className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
-      <a href="#section1" onClick={toggleMenu}>Seção 1</a>
-      <a href="#section2" onClick={toggleMenu}>Seção 2</a>
-      <a href="#section3" onClick={toggleMenu}>Seção 3</a>
-      <a href="#section4" onClick={toggleMenu}>Seção 4</a>
-      <a href="#section5" onClick={toggleMenu}>Seção 5</a>
-      <a href="#section6" onClick={toggleMenu}>Seção 6</a>
-      <a href="#section7" onClick={toggleMenu}>Seção 7</a>
-      <a href="#section8" onClick={toggleMenu}>Seção 8</a>
-      <a href="#section9" onClick={toggleMenu}>Seção 9</a>
-      <a href="#section10" onClick={toggleMenu}>Seção 10</a>
-      <a href="#section11" onClick={toggleMenu}>Seção 11</a>
-      <a href="#section12" onClick={toggleMenu}>Seção 12</a>
-      <a href="#section13" onClick={toggleMenu}>Seção 13</a>
-
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
       
+    ],
+    prevArrow: <button type="button" className="slick-prev">←</button>,
+    nextArrow: <button type="button" className="slick-next">→</button>
+  };
+
+  return (
+    <div className="App">
+      {isMenuOpen && <div className="overlay" onClick={closeMenu}></div>}
+      <div className="section section1">
+        
+        <div className="header">
+          <div className="menu-icon" onClick={toggleMenu}>
+            &#9776;
+          </div>
+          <div className="logo">
+            <h1>Mavic</h1>
+          </div>
+          <div className="contact-button">
+            <button onClick={() => alert('Contato')}>contate-nos</button>
+          </div>
+        </div>
+
+        <div className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+          <a href="#section1" onClick={toggleMenu}>Seção 1</a>
+          <a href="#section2" onClick={toggleMenu}>Seção 2</a>
+          <a href="#section3" onClick={toggleMenu}>Seção 3</a>
+          <a href="#section4" onClick={toggleMenu}>Seção 4</a>
+          <a href="#section5" onClick={toggleMenu}>Seção 5</a>
+          <a href="#section6" onClick={toggleMenu}>Seção 6</a>
+          <a href="#section7" onClick={toggleMenu}>Seção 7</a>
+          <a href="#section8" onClick={toggleMenu}>Seção 8</a>
+          <a href="#section9" onClick={toggleMenu}>Seção 9</a>
+          <a href="#section10" onClick={toggleMenu}>Seção 10</a>
+          <a href="#section11" onClick={toggleMenu}>Seção 11</a>
+          <a href="#section12" onClick={toggleMenu}>Seção 12</a>
+          <a href="#section13" onClick={toggleMenu}>Seção 13</a>
+        </div>
+
+        <div className="slide-controls">
+          {slides.map((_, index) => (
+            <div
+              key={index}
+              className={`dot ${currentSlide === index ? 'active' : ''}`}
+              onClick={() => setCurrentSlide(index)}
+            ></div>
+          ))}
+        </div>
+
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`slide ${currentSlide === index ? 'active' : ''}`}
+            style={{ backgroundImage: slide.image }}
+          >
+            <div className="slide-content">
+              <h2>{slide.text.title}</h2>
+              <p>{slide.text.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div id="section2" className="section section2">
+  <div className="slider-container">
+    <Slider {...sliderSettings}>
+      <div className="slide-item">
+        <div className="image-container">
+          <img src="/Imagens_Seção_02/Imagem_Cadeira_01.png" alt="Imagem Cadeira 1" />
+          <div className="circle-overlay"></div>
+        </div>
+        <h2>Nome da Categoria</h2>
+      </div>
+      <div className="slide-item">
+        <div className="image-container">
+          <img src="/Imagens_Seção_02/Imagem_Cadeira_02.png" alt="Imagem Cadeira 1" />
+          <div className="circle-overlay"></div>
+        </div>
+        <h2>Nome da Categoria</h2>
+      </div>
+      <div className="slide-item">
+        <div className="image-container">
+          <img src="/Imagens_Seção_02/Imagem_Cadeira_03.png" alt="Imagem Cadeira 1" />
+          <div className="circle-overlay"></div>
+        </div>
+        <h2>Nome da Categoria</h2>
+      </div>
+      <div className="slide-item">
+        <div className="image-container">
+          <img src="/Imagens_Seção_02/Imagem_Cadeira_04.png" alt="Imagem Cadeira 1" />
+          <div className="circle-overlay"></div>
+        </div>
+        <h2>Nome da Categoria</h2>
+      </div>
+      <div className="slide-item">
+        <div className="image-container">
+          <img src="/Imagens_Seção_02/Imagem_Cadeira_05.png" alt="Imagem Cadeira 1" />
+          <div className="circle-overlay"></div>
+        </div>
+        <h2>Nome da Categoria</h2>
+      </div>
+    </Slider>
+  </div>
+</div>
+
+<div id="section3" className="section section3">
+      <p>Em destaque</p>
+      <h1>Mais Vendidos</h1>
+      <div className="slider-container">
+        <Slider {...sliderSettings}>
+          <div className="slide-item">
+            <div className="image-container">
+              <img
+                src="/Imagens_Seção_02/Imagem_Cadeira_01.png"
+                alt="Imagem Cadeira 1"
+              />
+            </div>
+            <h2>Nome da Categoria</h2>
+            <p>Descrição do produto</p>
+          </div>
+          <div className="slide-item">
+            <div className="image-container">
+              <img
+                src="/Imagens_Seção_02/Imagem_Cadeira_02.png"
+                alt="Imagem Cadeira 2"
+              />
+            </div>
+            <h2>Nome da Categoria</h2>
+            <p>Descrição do produto</p>
+          </div>
+          <div className="slide-item">
+            <div className="image-container">
+              <img
+                src="/Imagens_Seção_02/Imagem_Cadeira_03.png"
+                alt="Imagem Cadeira 3"
+              />
+            </div>
+            <h2>Nome da Categoria</h2>
+            <p>Descrição do produto</p>
+          </div>
+          <div className="slide-item">
+            <div className="image-container">
+              <img
+                src="/Imagens_Seção_02/Imagem_Cadeira_04.png"
+                alt="Imagem Cadeira 4"
+              />
+            </div>
+            <h2>Nome da Categoria</h2>
+            <p>Descrição do produto</p>
+          </div>
+          <div className="slide-item">
+            <div className="image-container">
+              <img
+                src="/Imagens_Seção_02/Imagem_Cadeira_05.png"
+                alt="Imagem Cadeira 5"
+              />
+            </div>
+            <h2>Nome da Categoria</h2>
+            <p>Descrição do produto</p>
+          </div>
+        </Slider>
+      </div>
     </div>
 
-    <div className="slide-controls">
-  {slides.map((_, index) => (
-    <div
-      key={index}
-      className={`dot ${currentSlide === index ? 'active' : ''}`}
-      onClick={() => setCurrentSlide(index)}
-    ></div>
-  ))}
-</div>
-
-    {slides.map((slide, index) => (
-      <div
-        key={index}
-        className={`slide ${currentSlide === index ? 'active' : ''}`}
-        style={{ backgroundImage: slide.image }}
-      >
-        <div className="slide-content">
-          <h2>{slide.text.title}</h2>
-          <p>{slide.text.description}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-
-  <div id="section2" className="section section2">
-    <h1>Seção 2</h1>
-    <p>Conteúdo da segunda seção.</p>
-  </div>
-
-  <div id="section3" className="section section3">
-    <h1>Seção 3</h1>
-    <p>Conteúdo da terceira seção.</p>
-  </div>
-
-  <div id="section4" className="section section4">
-    <h1>Seção 4</h1>
-    <p>Conteúdo da quarta seção.</p>
-    <p>ERICK GATÃO E BONITÃO</p>
-  </div>
-
-  <div id="section5" className="section section5">
-    <h1>Seção 5</h1>
-    <p>Conteúdo da segunda seção.</p>
-  </div>
-
-  <div id="section6" className="section section6">
-    <h1>Seção 6</h1>
-    <p>Conteúdo da terceira seção.</p>
-  </div>
-
-  <div id="section7" className="section section7">
-    <h1>Seção 7</h1>
-    <p>Conteúdo da setima seção.</p>
-    <p> ERICK melhor que Ismael</p>
-  </div>
-
-  <div id="section8" className="section section8">
-    <h1>Seção 8</h1>
-    <p>Conteúdo da oitava seção.</p>
-  </div>
-
-  <div id="section9" className="section section9">
-    <h1>Seção 9</h1>
-    <p>Conteúdo da nona seção</p>
-  </div>
-
-  <div id="section10" className="section section10">
-    <h1>Seção 10</h1>
-    <p>Conteúdo da quarta seção.</p>
-    <p> ERICK Gostoso</p>
-  </div>
-
-  <div id="section11" className="section section11">
-    <h1>Seção 11</h1>
-    <p>Conteúdo da décima primeira.</p>
-  </div>
-
-  <div id="section12" className="section section12">
-    <h1>Seção 12</h1>
-    <p>Conteúdo da 12 seção.</p>
-  </div>
-
-  <div id="section13" className="section section13">
-    <h1>Seção 13</h1>
-    <p>Conteúdo da 13 seção.</p>
-    <p> ISMAEL cagão</p>
-  </div>
-  
-</div>
+    </div>
   );
 }
 
